38b02022f6d75dd0c3fc389eed28aa779a85fd48
DHYEY2312 <118186178+DHYEY2312@users.noreply.github.com>
2023-08-20 19:23:10 +0530

    code2.java

---
+++ Day2/code2.java
@@ -0,0 +1,11 @@
+public class helloworld {
+    
+    public static void main(String[] args) {
+
+        System.out.print("Hello world");
+        
+        System.out.println("Hello world");
+
+        System.out.print("Hello world");
+    }
+}
