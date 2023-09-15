77a5517147b9a16bf8d8844595f11eb541fa9b64
DHYEY2312 <118186178+DHYEY2312@users.noreply.github.com>
2023-09-15 12:32:09 +0530

    Revert "Update helloworld.java"

--- Day1/helloworld.java
+++ Day1/helloworld.java
@@ -3,6 +3,5 @@ public class helloworld {
     public static void main(String[] args) {
         
         System.out.println("Welcome to Java Programming");
-        System.out.println("Hello Harshul");
     }
 }
